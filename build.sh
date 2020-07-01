b1684a857278b3bec46c02c3c39da7105d8986c6
Your Name <you@example.com>
2020-07-01 20:11:05 +0200

    add jdk aarch64

--- build.sh
+++ build.sh
@@ -66,6 +66,7 @@ echo
 
 
 # DEPENDENCIES - KOPANO-CORE
+build makepkgs/jdk
 build makepkgs/php-xapian
 build makepkgs/python-sleekxmpp
 build makepkgs/python2-minimock
@@ -73,7 +74,7 @@ build makepkgs/python2-minimock
 build makepkgs/libvmime
 build makepkgs/libical2
 build makepkgs/python2-tlslite
-build makepkgs/jdk
+
 
 # DEPENDENCIES -
 # KOPANO-WEBAPP
