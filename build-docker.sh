f741e1d06faafa7e38258a3f33695d9ec08212e7
Your Name <you@example.com>
2020-12-02 17:17:36 +0100

    resolve remote image

--- build-docker.sh
+++ build-docker.sh
@@ -4,9 +4,11 @@ echo
 echo "# CREATE BUILD ENVIRONMENT"
 echo
 
-# Use version or try using branchname as version
+
+
 if [ -z "$VERSION" ];
 then
+    echo "# RESOLVE VERSION"
     if BRANCH_CURRENT=$(git branch --show-current) \
     && [ "$BRANCH_CURRENT" != "master" ];
     then
@@ -16,18 +18,33 @@ then
     fi
 fi
 
+
+if [ ! -z "$DOCKER_REMOTE_REGISTRY"];
+then
+    echo "# PULL DOCKER IMAGE"
+    remoteImage="$DOCKER_REMOTE_REGISTRY/$(uname -m)/archlinux-basedevel:$VERSION"
+    localImage="$(uname -m)/archlinux-basedevel:$VERSION"
+
+    if docker pull $remoteImage;
+    then
+	docker tag $remoteImage $localImage
+    fi
+fi
+
+
 if [ -z "$(docker image ls -q $(uname -m)/archlinux-basedevel:$VERSION)" ];
 then
-	if [ -z "$(docker image ls -q $(uname -m)/archlinux:$VERSION)" ];
-	then
-		cd docker/archlinux-docker
-		make docker-image VERSION="$VERSION"
-		cd ../..
-	fi
-	
-	cd docker/archlinux-basedevel-docker
+    echo "# BUILD DOCKER IMAGE"
+    if [ -z "$(docker image ls -q $(uname -m)/archlinux:$VERSION)" ];
+    then
+	cd docker/archlinux-docker
 	make docker-image VERSION="$VERSION"
 	cd ../..
+    fi
+
+    cd docker/archlinux-basedevel-docker
+    make docker-image VERSION="$VERSION"
+    cd ../..
 fi
 
 docker/archlinux-basedevel-docker/run.sh ./build.sh
