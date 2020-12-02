8a063c1da3df6aa756e72b5b5722130cab8fb963
Marti McFly <post@martinapietruschka.de>
2020-12-02 16:32:18 +0100

    Update build-docker.sh
    build docker container if missing

--- build-docker.sh
+++ build-docker.sh
@@ -9,15 +9,15 @@ then
 	VERSION="latest"
 fi
 
-if [ -z "$(docker image ls -q $(uname -m)/archlinux:$VERSION)" ];
-then
-	cd docker/archlinux-docker
-	make docker-image VERSION="$VERSION"
-	cd ../..
-fi
-
 if [ -z "$(docker image ls -q $(uname -m)/archlinux-basedevel:$VERSION)" ];
 then
+	if [ -z "$(docker image ls -q $(uname -m)/archlinux:$VERSION)" ];
+	then
+		cd docker/archlinux-docker
+		make docker-image VERSION="$VERSION"
+		cd ../..
+	fi
+	
 	cd docker/archlinux-basedevel-docker
 	make docker-image VERSION="$VERSION"
 	cd ../..
