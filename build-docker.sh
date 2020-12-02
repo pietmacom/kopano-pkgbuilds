6a120785b12baedbc117f43607133a51b4539281
Marti McFly <post@martinapietruschka.de>
2020-12-02 16:34:29 +0100

    Update build-docker.sh
    check for existence

--- build-docker.sh
+++ build-docker.sh
@@ -11,10 +11,13 @@ fi
 
 if [ -z "$(docker image ls -q $(uname -m)/archlinux-basedevel:$VERSION)" ];
 then
-	cd docker/archlinux-docker
-	make docker-image VERSION="$VERSION"
-	cd ../..
-
+	if [ -z "$(docker image ls -q $(uname -m)/archlinux:$VERSION)" ];
+	then
+		cd docker/archlinux-docker
+		make docker-image VERSION="$VERSION"
+		cd ../..
+	fi
+	
 	cd docker/archlinux-basedevel-docker
 	make docker-image VERSION="$VERSION"
 	cd ../..
