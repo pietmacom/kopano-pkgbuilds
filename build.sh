ac1335040d2ea2cecda78454df30b29be077a2b4
Marti McFly <447353+mpietruschka@users.noreply.github.com>
2023-01-17 14:29:39 +0100

    Update build.sh
    test

--- build.sh
+++ build.sh
@@ -145,8 +145,8 @@ makepkgs=(
     # CORE
     'libiconv#nosync#nogit'
     'swig#nosync#nogit'
-#    'libvmime#nosync'
-    'kopano-libvmime'
+    'libvmime#nosync'
+#    'kopano-libvmime'
     'php74#nosync#nogit'
     'php-binding#nosync#nogit'
     'kopano-core'
